f5700517dc4709126ebde9a7d215b5f5f3aa9254
Mr.Lutfulla <murodjonovlutfulla@gmail.com>
2022-11-23 09:14:06 +0500

    i entered the data via javascript

---
+++ jscript.js
@@ -0,0 +1,12 @@
+let obj= {
+    myname: "Lutfulla",
+    mysurname:"Murodjonov",
+    age: "20",
+    conditon: "i am not married",
+    myfavouritecolor: "red, green, blue",
+    number:"+998906168895",
+    hobby:"i like watch harror film",
+    relative: " i have my mather, my father, my sester, brather, also my high-sester and little sister",
+
+}
+console.log(obj)
