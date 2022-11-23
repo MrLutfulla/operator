ef2e79c17a833ed32bfaf5a9e16965288d65f642
Mr.Lutfulla <murodjonovlutfulla@gmail.com>
2022-11-23 10:24:43 +0500

    my js code in js, homework

--- jscript.js
+++ jscript.js
@@ -10,3 +10,13 @@ let obj= {
 
 }
 console.log(obj)
+
+let arr = [["Lutfulla", "Murodjonov"], [20, 3, 906168895],  ["red","green","blue"]]
+console.log(arr[0][0])
+
+
+
+let prom = prompt("Son kiriting");
+let prom2= prompt("Keyingi son kiriting");
+console.log(Number(prom) % Number(prom2));
+console.log( "my age "+ prom+" "+ "your age "+ prom2);
